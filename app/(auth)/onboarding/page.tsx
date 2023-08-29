b45e0ebe89d9f5400e345d9b7dcfd5cd8c457bec
guiavicar <guiavicar777@gmail.com>
2023-08-29 16:22:08 -0300

    onboarding

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+async function Page(){
+    return(
+        <main>
+            <h1 className="head-Text">
+                Onboarding
+
+            </h1>
+        </main>
+    )
+}
+export default Page;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>ABRA CADABRA PORRAAA</h1>
+    </main>
+  )
+}
